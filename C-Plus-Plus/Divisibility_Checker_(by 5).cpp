856cfc04ef5735088eb658d11a936229dbfbf3b3
ItzPranav61 <pranavsawant610@gmail.com>
2025-11-12 18:56:25 +0530

    Add program to check divisibility by 5 using C++.
    This simple C++ program checks whether a given integer is divisible by 5.
    It takes an integer input from the user and uses the modulus operator (%) to determine if the number leaves a remainder when divided by 5.
    
    If the remainder is 0, it displays that the number is divisible by 5; otherwise, it reports that it is not divisible by 5.
    
    💡 Features:
    
    * Accepts user input via standard input (cin)
    
    * Uses conditional statements (if / else) for logic checking
    
    * Demonstrates basic use of the modulus operator and console I/O in C++
    
     Example Output:
    Enter your number: 25
    25 is divisible by 5!
    
    Enter your number: 23
    23 is not divisible by 5.
     Concepts Used:
    
    * Input/Output in C++
    
    * Conditional Statements
    
    * Arithmetic Operators

---
+++ C-Plus-Plus/Divisibility_Checker_(by 5).cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter your number: ";
+    cin>>num;
+
+    if ((num%5)==0)
+    {
+        cout<<num<<" is divisible by 5!"; 
+    }
+    else 
+    {
+        cout<<num<<" is not divisible by 5.";
+    }
+    return 0;
+}
